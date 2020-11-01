bee7c8227b0b803599dbd35f077d42cebf31c24c
Luda-L <jesustark0235@2gmail.com>
2020-11-02 01:57:58 +0900

    lus

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,17 +6,65 @@
 /*   By: mtak <mtak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 01:57:13 by mtak              #+#    #+#             */
-/*   Updated: 2020/11/01 03:30:47 by mtak             ###   ########.fr       */
+/*   Updated: 2020/11/01 22:59:42 by mtak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+int ft_strlen(char *str);
+void ft_strcat(char *result, char **strs, int tmp, char sep);
+
 char *ft_strjoin(int size, char **strs, char *sep)
 {
-	int len;
-	int *arr
+	char *result;
+	int tot_len;
+	int tmp;
+	
+	tot_len = 0;
+	tmp = size;
+	while(size >= 0)
+	{
+		tot_len += ft_strlen(strs[size - 1]);
+		size--;
+	}
+	tot_len += tmp;
+	result  = (char *)malloc(sizeof(char) * tot_len);
+	ft_strcat(result, strs, tmp, *sep);
+	result[tot_len - 1] = '\0';
+	return (result);
+}
 
-	len  = 2*size - 1;
+int ft_strlen(char *str)
+{
+	int cnt;
+
+	cnt = 0;
+	while(str[cnt])
+		cnt++;
+	return (cnt);
+}
+
+void ft_strcat(char *result, char **strs, int tmp, char sep)
+{
+	int i;
+	int j;
+	int k;
+	
+	i = 0;
+	j = 0;
+	while(j < tmp)
+	{
+		k = 0;
+		while(strs[j][k])
+		{
+			result[i] = strs[j][k];
+			i++;
+			k++;
+		}
+		result[i++] = sep;
+		j++;
+	}
 	
 }
+
